e026727f915318d9ebb618accc04483889cd6935
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-22 16:02:24 +0400

    Edit schema file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions', 
         registrations: 'users/registrations'
-      }
- 
+      } 
+  resources :users, only: [:index]   
   root to: "users#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,8 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_05_22_083031) do
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
