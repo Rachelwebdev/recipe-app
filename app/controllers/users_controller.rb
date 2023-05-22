83ecc35ae930fe40e812122b8b7071c95e015bd5
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-22 16:06:25 +0400

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    def index
-        # @users = User.all(created_at: :asc)
-    end
+  def index
+    # @users = User.all(created_at: :asc)
+  end
 end
