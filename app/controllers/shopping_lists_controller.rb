6870da072002a9067d8c9c5e4a995ed30f06a652
CHRIS <NWAKAUC1@GMAIL.COM>
2023-05-24 18:24:43 +0200

    Implement shopping -list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,17 +1,13 @@
 class ShoppingListsController < ApplicationController
   def index
     @user = current_user
-   # @recipe = Recipe.where(user: @user)
-    #@ingredients = RecipeFood.includes(:food).where(recipe: @recipe).group_by { |ingredient| ingredient.food.name }
+    @recipe = Recipe.where(user: @user)
+    @ingredients = RecipeFood.includes(:food).where(recipe: @recipe).group_by { |ingredient| ingredient.food.name }
 
-    #@total_price = @ingredients.map do |_food, ingredients|
-     # ingredients.map do |ingredient|
-     #   ingredient.food.price * ingredient.quantity
-      #end.sum
-    #end.sum
-  end
-
-  def show
-    @shopping_list = ShoppingList.find(params[:id])
+    @total_price = @ingredients.map do |_food, ingredients|
+      ingredients.map do |ingredient|
+        ingredient.food.price * ingredient.quantity
+      end.sum
+    end.sum
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,16 @@ Rails.application.routes.draw do
       }
 
   root "foods#index"
+  get '/shopping_lists', to: 'shopping_lists#index'
+
 
   resources :users, only: [:index, :show] do
     resources :foods, only: [:index, :new, :create, :destroy] do
-      delete ":id", to: "foods#destroy", on: :member     
+           
     end
   end
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
     # delete "id", to: "recipes#destroy", on: :member
   end
+  
 end
