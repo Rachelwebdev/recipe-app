9f7bb91320d6725a78331478255023af7a98ff88
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-26 12:19:19 +0400

    Edit the user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_25_085641) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_26_081550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,8 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_25_085641) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
