d8202f2913009768b69c7fa1e05ea456a2897e1a
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-27 10:47:28 +0400

    delete a migration file

--- db/migrate/20230526081550_remove_first_name_and_rename_last_name_to_name.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFirstNameAndRenameLastNameToName < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :first_name
-    rename_column :users, :last_name, :name
-  end
-end
